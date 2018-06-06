c239b78f080d4b4916113fecf4d998037f0c38d2
modelorona <angel.hristozov@gmail.com>
2018-06-06 20:06:14 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="keel",
+    packages=['keel'],
+    version='0.1',
+    description='Kill proccesses effectively and easily',
+    author='Anguel Hristozov',
+    author_email='angel.hristozov@gmail.com',
+    url='https://github.com/modelorona/keel',
+    download_url='https://github.com/modelorona/keel/archive/0.1.tar.gz',
+    keyword=['kill', 'process', 'easy'],
+    classifiers=[]
+)
